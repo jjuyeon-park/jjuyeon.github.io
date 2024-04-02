1b5e41249da3f19432cd1ea44a76f254cbb42139
juyeon_coding <104187704+jjuyeon-park@users.noreply.github.com>
2024-04-03 00:43:11 +0900

    17938. 문자열 세 개
    17938. 문자열 세 개

---
+++ 17938. 문자열 세 개/pb6_string3.cpp
@@ -0,0 +1,23 @@
+//17938. 문자열 세 개
+//https ://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=4&contestProbId=AYmRMhpakYADFARi&categoryId=AYmRMhpakYADFARi&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=4&pageSize=10&pageIndex=1
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int T;
+	int test_case;
+
+	freopen_s(new FILE*, "text.txt", "r", stdin);
+
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; test_case++) {
+		
+
+
+	}
+}
